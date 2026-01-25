5748605c362c9d55b49e4d50a4ff36fe3dc98ced
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-01-25 12:56:04 +0700

    Update casmon.sh

--- dev/raw/casmon.sh
+++ dev/raw/casmon.sh
@@ -1,1 +1,16 @@
 #!/bin/env bash
+(1) install packages
+function casmon_install_packages_libv() {
+
+}
+
+function casmon_install_packages_qemu() {
+
+}
+function casmon_install_packages() {
+casmon_install_packages_qemu
+casmon_install_packages_libv
+}
+
+(2) download files qcow
+(3) configs installer
