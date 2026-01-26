f096a3433232339f3150f87d56dba957dbededc4
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-01-26 10:42:06 +0700

    update

--- dev/raw/casmon.sh
+++ dev/raw/casmon.sh
@@ -10,6 +10,10 @@
 ## revision by null @ 26
 #--[1] option dialog
 function casmon_dialog() {
+  options=$1
+  options_deploy=$2
+  options_master=$2
+
   casmon_deploy
   casmon_master
 }
@@ -17,7 +21,7 @@ function casmon_dialog() {
 #-[1] qcow management
 #-[2][1] download qcow
 function casmon_master_takers() {
-   if [[ $options != "master_takers" ]]; then
+   if [[ $options_master != "master_takers" ]]; then
     return
     fi
 }
@@ -25,7 +29,7 @@ function casmon_master_takers() {
 
 #-[2][2] update qcow
 function casmon_master_update() {
-   if [[ $options != "master_update" ]]; then
+   if [[ $options_master != "master_update" ]]; then
     return
     fi
 }
@@ -33,7 +37,7 @@ function casmon_master_update() {
 #-[2][3] delete qcow
 function casmon_master_delete() {
 
-   if [[ $options != "master_delete" ]]; then
+   if [[ $options_master != "master_delete" ]]; then
     return
     fi
 }
@@ -55,7 +59,7 @@ function casmon_master() {
 #-[2](1) create host
 function casmon_deploy_create() {
 
-   if [[ $options != "deploy_create" ]]; then
+   if [[ $options_deploy != "deploy_create" ]]; then
     return
     fi
 }
@@ -63,34 +67,34 @@ function casmon_deploy_create() {
 
 #-[2](2) starts host
 function casmon_deploy_starts() {
- if [[ $options != "deploy_starts" ]]; then
+ if [[ $options_deploy != "deploy_starts" ]]; then
     return
     fi
 }
 
 #-[2](3) snaper host
 function casmon_deploy_snaper() {
-  if [[ $options != "deploy_snaper" ]]; then
+  if [[ $options_deploy != "deploy_snaper" ]]; then
     return
     fi
 }
 #-[2](4) delete host
 function casmon_deploy_delete() {
-if [[ $options != "deploy_delete" ]]; then
+if [[ $options_deploy != "deploy_delete" ]]; then
     return
     fi
 }
 
 #-[2](5) update host
 function casmon_deploy_update() {
-  if [[ $options != "deploy_update" ]]; then
+  if [[ $options_deploy != "deploy_update" ]]; then
     return
     fi
 }
 
 function casmon_deploy() {
   
-  if [[ $options != "master" ]]; then
+  if [[ $options != "deploy" ]]; then
     return
   fi
   casmon_deploy_create
@@ -102,7 +106,7 @@ function casmon_deploy() {
 
 
 function casmon() {
-  casmon_dialog
+  casmon_dialog $1 $2
 }
 
 
