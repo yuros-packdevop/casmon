e38c6b46f9296a0e9c9aa73d8fc5dd03e77960f9
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-01-30 20:40:58 +0700

    update

--- dev/raw/lib/mast.sh
+++ dev/raw/lib/mast.sh
@@ -8,15 +8,23 @@ function casmon_master_takers() {
   echo "list master"
 }
 
-#-[2][2] verify qcow
-function casmon_master_verify() {
+#-[2][1][1] verify qcow
+function casmon_master_takers_verify() {
    if [[ $options_master != "-ve" ]]; then
     return
     ficom
   echo "master terverifikasi"
 }
 
-#-[2][3] update qcow
+#-[2][1][2] compare qcow
+function casmon_master_takers_compar() {
+   if [[ $options_master != "-co" ]]; then
+    return
+    ficom
+  echo "master terverifikasi"
+}
+
+#-[2][2] update qcow
 function casmon_master_update() {
    if [[ $options_master != "-up" ]]; then
     return
@@ -24,7 +32,23 @@ function casmon_master_update() {
   echo "master update"
 }
 
-#-[2][4] delete qcow
+#-[2][2][1] verify qcow
+function casmon_master_update_verify() {
+   if [[ $options_master != "-uv" ]]; then
+    return
+    ficom
+  echo "master terverifikasi"
+}
+
+#-[2][2][2] compare qcow
+function casmon_master_update_compar() {
+   if [[ $options_master != "-uc" ]]; then
+    return
+    ficom
+  echo "master terverifikasi"
+}
+
+#-[2][3] delete qcow
 function casmon_master_delete() {
 
    if [[ $options_master != "-del" ]]; then
@@ -41,7 +65,10 @@ function casmon_master() {
   fi
   
     casmon_master_takers
-    casmon_master_verify
+    casmon_master_takers_verify
+    casmon_master_takers_compar
     casmon_master_update
+    casmon_master_update_verify
+    casmon_master_update_compar
     casmon_master_delete
 }
