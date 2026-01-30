6eb6331053a6514732bbc59db3b5f70a8a029416
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-01-30 20:03:13 +0700

    update

--- dev/raw/lib/mast.sh
+++ dev/raw/lib/mast.sh
@@ -8,43 +8,11 @@ function casmon_master_takers() {
   echo "list master"
 }
 
-#-[2][1](1) download qcow ubuntu
-function casmon_master_takers_ubuntu() {
-  if [[ $options_master_os != "-ub" ]]; then
-  return
-  fi
-  echo "master download ubuntu"
-}
-
-#-[2][1](2) download qcow debian
-function casmon_master_takers_debian() {
-  if [[ $options_master_os != "-de" ]]; then
-  return
-  fi
-  echo "master download debian"
-}
-
-#-[2][1](3) download qcow fedora
-function casmon_master_takers_fedora() {
-  if [[ $options_master_os != "-fe" ]]; then
-  return
-  fi
-  echo "master download fedora"
-}
-
-#-[2][1](3) download qcow arch
-function casmon_master_takers_arch() {
-  if [[ $options_master_os != "-ar" ]]; then
-  return
-  fi
-  echo "master download arch"
-}
-
 #-[2][2] verify qcow
 function casmon_master_verify() {
    if [[ $options_master != "-ve" ]]; then
     return
-    fi
+    ficom
   echo "master terverifikasi"
 }
 
@@ -73,10 +41,6 @@ function casmon_master() {
   fi
   
     casmon_master_takers
-    casmon_master_takers_ubuntu
-    casmon_master_takers_debian
-    casmon_master_takers_fedora
-    casmon_master_takers_arch
     casmon_master_verify
     casmon_master_update
     casmon_master_delete
